87ec5963b82ab93f89ada9ff067fe3d75671ddbe
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-26 15:05:51 -0600

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   variables/change_var.svelte

---
+++ variables/change_var.svelte
@@ -0,0 +1,16 @@
+<style>
+    * {
+        color: white;
+    }
+    button {
+        background-color: black;
+    }
+</style>
+<script>
+    let testVar = "Hello"
+    function func() {
+        testVar = "World!"
+    }
+</script>
+<h1>{testVar}</h1>
+<button on:click={func}>h</button>
