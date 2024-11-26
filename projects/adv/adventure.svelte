8199ea5aa33033000d2ed6540d0e01abb0561c8c
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-26 15:17:04 -0600

    new file:   projects/adv/adventure.svelte

---
+++ projects/adv/adventure.svelte
@@ -0,0 +1,22 @@
+<style>
+    * {
+        color: white;
+    }
+    button {
+        background-color: black;
+    }
+</style>
+<script>
+    let fate = ""
+    function path1() {
+        fate = "You enjoy making websites more!"
+    }
+    function path2() {
+        fate = "You still enjoy making sites, just a little less."
+    }
+</script>
+<h1>Svelte Adventure!</h1>
+<button on:click={path1}>Svelte</button>
+<button on:click={path2}>Plain HTML</button>
+<h2>Your Fate</h2>
+<p>{fate}</p>
